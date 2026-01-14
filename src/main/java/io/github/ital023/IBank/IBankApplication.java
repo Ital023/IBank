2019f3b1d5e6dc6476ec0b5543ce2945d88f104a
Ital023 <contadoitalo911@gmail.com>
2026-01-13 23:12:15 -0300

    init: essential files

---
+++ src/main/java/io/github/ital023/IBank/IBankApplication.java
@@ -0,0 +1,13 @@
+package io.github.ital023.IBank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IBankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/ital023/IBank/IBankApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.ital023.IBank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
