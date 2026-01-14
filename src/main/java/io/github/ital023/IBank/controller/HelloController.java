d8351a530fabe151aa53d2cf490f6bd54cc45203
Ital023 <contadoitalo911@gmail.com>
2026-01-14 15:21:30 -0300

    feat: Audit interceptor

---
+++ src/main/java/io/github/ital023/IBank/controller/HelloController.java
@@ -0,0 +1,16 @@
+package io.github.ital023.IBank.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping
+    public ResponseEntity<String> ok(){
+        System.out.println("inside controller");
+        return ResponseEntity.ok().build();
+    }
+
+}
---
+++ src/main/java/io/github/ital023/IBank/inteceptors/InterceptorConfig.java
@@ -0,0 +1,22 @@
+package io.github.ital023.IBank.inteceptors;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class InterceptorConfig implements WebMvcConfigurer {
+
+    private final AuditInterceptor auditInterceptor;
+
+    public InterceptorConfig(AuditInterceptor auditInterceptor) {
+        this.auditInterceptor = auditInterceptor;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(auditInterceptor)
+                .order(0);
+    }
+
+}
