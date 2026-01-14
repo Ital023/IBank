2edbaa5f7f31efa9aa93b5d0d9b5df27a8b1f987
Ital023 <contadoitalo911@gmail.com>
2026-01-13 23:21:16 -0300

    feat: Transfer Entity

---
+++ src/main/java/io/github/ital023/IBank/entities/Transfer.java
@@ -0,0 +1,31 @@
+package io.github.ital023.IBank.entities;
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+public class Transfer {
+
+    @Id
+    @Column(name = "transfer_id")
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID transferId;
+
+    @ManyToOne
+    @JoinColumn(name = "wallet_receiver_id")
+    private Wallet receiver;
+
+    @ManyToOne
+    @JoinColumn(name = "wallet_sender_id")
+    private Wallet sender;
+
+    @Column(name = "transfer_value")
+    private BigDecimal transferValue;
+
+    @Column(name = "transfer_date_time")
+    private LocalDateTime transferDateTime;
+
+}
