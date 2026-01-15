68fcd4153f9570dc562965276c1538a33c494af6
Ital023 <contadoitalo911@gmail.com>
2026-01-15 16:48:47 -0300

    feat: Deleting a wallet

--- src/main/java/io/github/ital023/IBank/controller/WalletController.java
+++ src/main/java/io/github/ital023/IBank/controller/WalletController.java
@@ -4,12 +4,10 @@ import io.github.ital023.IBank.controller.dto.CreateWalletDto;
 import io.github.ital023.IBank.service.WalletService;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
+import java.util.UUID;
 
 @RestController
 @RequestMapping(path = "/wallets")
@@ -26,6 +24,13 @@ public class WalletController {
         var wallet = walletService.createWallet(dto);
 
         return ResponseEntity.created(URI.create("/wallets/" + wallet.getWalletId().toString())).build();
+    }
+
+    @DeleteMapping(path = "/{walletId}")
+    public ResponseEntity<Void> deleteWallet(@PathVariable("walletId")UUID walletId) {
+        var deleted = walletService.deleteWallet(walletId);
+
+        return deleted ? ResponseEntity.noContent().build() : ResponseEntity.notFound().build();
     }
 
 }
--- src/main/java/io/github/ital023/IBank/service/WalletService.java
+++ src/main/java/io/github/ital023/IBank/service/WalletService.java
@@ -2,11 +2,13 @@ package io.github.ital023.IBank.service;
 
 import io.github.ital023.IBank.controller.dto.CreateWalletDto;
 import io.github.ital023.IBank.entities.Wallet;
+import io.github.ital023.IBank.exception.DeleteWalletException;
 import io.github.ital023.IBank.exception.WalletDataAlreadyExistsException;
 import io.github.ital023.IBank.repository.WalletRepository;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
+import java.util.UUID;
 
 @Service
 public class WalletService {
@@ -34,4 +36,20 @@ public class WalletService {
 
         return walletRepository.save(wallet);
     }
+
+    public boolean deleteWallet(UUID walletId) {
+        var wallet = walletRepository.findById(walletId);
+
+        if(wallet.isPresent()) {
+
+            if(wallet.get().getBalance().compareTo(BigDecimal.ZERO) != 0) {
+                throw new DeleteWalletException("the balance isn't zero, the current amount is $" + wallet.get().getBalance());
+            }
+
+            walletRepository.deleteById(walletId);
+        }
+
+        return wallet.isPresent();
+    }
+
 }
