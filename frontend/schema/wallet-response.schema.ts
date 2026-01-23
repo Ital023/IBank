903b93caefbba5ba52d402993386ed769dd91eea
Ital023 <contadoitalo911@gmail.com>
2026-01-22 22:35:18 -0300

    feat: Wallet Response Schema

---
+++ frontend/schema/wallet-response.schema.ts
@@ -0,0 +1,12 @@
+import z from "zod";
+import { WalletSchema } from "./wallet.schema";
+import { StatementSchema } from "./statement.schema";
+import { PaginationSchema } from "./pagination.schema";
+
+const WalletResponseSchema = z.object({
+  wallet: WalletSchema,
+  statements: z.array(StatementSchema),
+  pagination: PaginationSchema
+})
+
+export type WalletResponse = z.infer<typeof WalletResponseSchema>
